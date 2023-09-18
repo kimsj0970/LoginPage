905eb7802270dadb47e9fb1a1480292d44c3de1c
kimsj0970 <53886275+kimsj0970@users.noreply.github.com>
2023-09-18 12:50:58 +0900

    회원가입 오류수정

--- my-app/pages/api/make/server.tsx
+++ my-app/pages/api/make/server.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/DBconnect";
+import { serialize } from 'cookie';
 
 type Member = {
     userid:string,
@@ -7,14 +8,21 @@ type Member = {
     }
 
     export default function handler(req:any,res:any):any{
+      const cookieOptions = {
+        maxAge: 30 * 24 * 60 * 60,  //쿠키 유효 기간 (30일)
+        path: '/', // 모든 경로에서 쿠키 접근 가능
+        httpOnly: true, // JavaScript를 통해 쿠키에 접근 불가
+      };
         if(req.method=='POST'){
             let {userid, email,password}:Member = req.body
             try {
                 // 데이터베이스 쿼리 실행
                 db.query('INSERT INTO user (userid,email,password) values(?,?,?)', [userid, email, password], (error: any, results: Member) => {
                   if (error) {
-                    res.status(500).json('DB 저장 에러');
+                    res.status(500).json('중복된 아이디가 존재합니다');
+                    //res.status(200).redirect(301,'/JoinPage')
                   } else {
+                    res.setHeader('Set-Cookie', serialize('userid', userid, cookieOptions));
                     res.status(200).redirect(301,'/SuccessPage')
                   }
                 });
